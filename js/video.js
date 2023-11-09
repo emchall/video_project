6eaaddb852e23de8a93b93980ce9be3d97f462a6
emchall <143909433+emchall@users.noreply.github.com>
2023-11-08 21:36:39 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,11 +1,15 @@
-var video;
+var video = document.getElementById("player1");
+
 
 window.addEventListener("load", function() {
+	video.autoplay=false;
+	video.loop=false;
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	video.play();
+	console.log("Play Video");
+ });
 
